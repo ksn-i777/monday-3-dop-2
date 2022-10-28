26095c3f5f93ec83bb75870002ddd90b2cb60519
ksn-i777 <ksn_i777@mail.ru>
2022-10-28 19:33:17 +0300

    dop-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from './components/Button';
+
+type ShowType = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: false,
+}
 
 function App() {
+
+  const [show, setShow] = useState<Array<ShowType>>([])
+
+  
+
+  const useShowUp = () => {
+    //useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setShow(json))
+    //}, [])
+  }
+
+  const clean = () => {
+    setShow([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button name={'Show me'} callBack={useShowUp}/>    
+      <Button name={'Clean me'} callBack={clean}/>  
+      <ul>
+        {show.map((el) => {
+          return (
+            <li key={el.id}>
+              <span>{el.id}</span>
+              <span>{el.title}</span>
+              <span>{`${el.completed}`}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
